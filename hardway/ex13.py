68c268de0e0c6b4eb79d3905852575e499e979b4
js19931018 <1736219667@qq.com>
2017-09-20 15:29:45 +0800

    yes

--- hardway/ex13.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-script, first, second, third = argv
-print "The script is called:", script
-print "Your first variable is:", first
-print "Your second variable is:", second
-print "Your third variable is:", third
-#only for shell
-# python ex13.py xx xx xx
